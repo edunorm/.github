0f0b5b240e5024c95091ec23ce28b52fef960e2a
Mia <mia@mia.cx>
2025-05-13 21:18:53 +0200

    remove husky precommit hook

--- packages/config/src/lib/types/prettier.ts
+++
@@ -1,7 +0,0 @@
-import { Options } from "prettier";
-
-export type Override = {
-	files: string | string[];
-	excludeFiles?: string | string[];
-	options?: Options;
-};
--- prettier.config.mjs
+++
@@ -1,6 +0,0 @@
-import base from "@canvas-ac/config/prettier/base";
-
-/** @type {import("prettier").Config} */
-export default {
-	...base,
-}
